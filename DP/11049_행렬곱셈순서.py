5085c91e527c122ee83d55f506604b0e18afc39b
Nomemb <nkj1205@gmail.com>
2022-05-10 22:55:32 +0900

    Create 11049_행렬곱셈순서.py

---
+++ DP/11049_행렬곱셈순서.py
@@ -0,0 +1,14 @@
+import sys
+input = sys.stdin.readline
+N = int(input())
+p = [list(map(int, input().split())) for i in range(N)]
+m = [[0] * N for i in range(N)]
+
+for i in range(1,N):
+    for j in range(N-i):
+        x = i+j
+        m[j][x] = 2 ** 32
+        for k in range(j,x):
+            m[j][x] = min(m[j][x], m[j][k] + m[k+1][x] + p[j][0]*p[k][1]*p[x][1])
+
+print(m[0][N-1])
