55a69ff58aa6c1187d8dbab6f694ac5fcce71777
Nomemb <nkj1205@gmail.com>
2022-07-19 22:42:52 +0900

    Create 11403_경로찾기.py

---
+++ 그래프/플로이드-워셜/11403_경로찾기.py
@@ -0,0 +1,20 @@
+from collections import deque
+from sys import stdin
+
+n = int(input())
+
+visited = []
+for _ in range(n):
+    l = list(map(int, stdin.readline().split()))
+    visited.append(l)
+
+
+for k in range(n):
+    for i in range(n):
+        for j in range(n):
+            if visited[i][k] and visited[k][j]:
+                visited[i][j] = 1
+                
+            
+for i in range(n):
+    print(*visited[i][:])     
