2bcdc0b2451c397ea9d831bfab8b831df3984316
Nomemb <nkj1205@gmail.com>
2022-07-21 11:50:33 +0900

    Create 15988_1,2,3더하기3.py

---
+++ DP/15988_1,2,3더하기3.py
@@ -0,0 +1,16 @@
+from cProfile import label
+
+
+T = int(input())
+DP = [0 for i in range(1000001)]
+DP[0],DP[1],DP[2] = 1,1,2
+l = []
+for i in range(T):
+    n = int(input())
+    l.append(n)
+
+for i in range(3,max(l)+1):
+    DP[i] = (DP[i-1]+DP[i-2]+DP[i-3])%1000000009
+for i in l:
+    print(DP[i])
+
