db737d1bc2d58fcebadf9adf06dfb9f32bd086cd
Nomemb <nkj1205@gmail.com>
2022-07-27 23:54:02 +0900

    Create 6064_카잉달력.py

---
+++ 6064_카잉달력.py
@@ -0,0 +1,23 @@
+from math import gcd
+
+def LCM(x,y):
+    return x*y // gcd(x,y)
+
+def solve(M,N,x,y):
+    r = LCM(M,N)
+    y %= N
+    for i in range(r//M+1):
+        ans = (i * M + x)
+        if ans % N == y:
+            return ans
+    return -1
+
+
+T = int(input())
+
+for i in range(T):
+    M,N,x,y = map(int,input().split())
+    print(solve(M,N,x,y))
+
+    
+
